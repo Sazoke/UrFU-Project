84f75aaf95557914405ef2d76bc2e1e99095e1e8
Sazoke <44672298+Sazoke@users.noreply.github.com>
2019-06-28 00:03:42 +0500

    ver 3

---
+++ UrFU_Prokect/UrFU_Prokect/Elements.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.IO;
+
+namespace Elements
+{
+    public class Buttons : IEnumerable
+    {
+        private List<Button> buttons;
+        public Buttons()
+        {
+            buttons = new List<Button>();
+            var left = 200;
+            var right = 200;
+            for (int i = 0; i < 3; i++)
+            {
+                var newButton = new Button();
+                newButton.Margin = new System.Windows.Thickness(left, 50 + 100 * i, right, 300 - 100 * i);
+                newButton.Content = "Text " + (i + 1);
+                newButton.FontSize = 20;
+                buttons.Add(newButton);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var button in buttons)
+                yield return button;
+        }
+    }
+
+    public class MyTextBox
+    {
+        private TextBox textBox;
+        public MyTextBox()
+        {
+            textBox = new TextBox();
+            textBox.Margin = new System.Windows.Thickness(50, 250, 50, 20);
+            textBox.TextWrapping = System.Windows.TextWrapping.Wrap;
+        }
+
+        public void Focus() => textBox.Focus();
+        public void SetChager(TextChangedEventHandler action) => textBox.TextChanged += action;
+        public void Show(Grid grid) => grid.Children.Add(textBox);
+        public string Text => textBox.Text;
+    }
+
+    public class MyTextBlock
+    {
+        public int wrongs;
+        private string beginText;
+        private TextBlock textBlock;
+        public MyTextBlock()
+        {
+            textBlock = new TextBlock();
+            textBlock.Margin = new System.Windows.Thickness(50, 50, 50, 250);
+        }
+
+        public void SetText(char numberOfText)
+        {
+            var str = new StreamReader(@"D:\Projects\UrFu Practic\UrFU-Project\UrFU_Prokect\Текст" + numberOfText + ".txt");
+            textBlock.Text = str.ReadToEnd();
+            beginText = textBlock.Text.ToString();
+            textBlock.TextWrapping = System.Windows.TextWrapping.Wrap;
+        }
+
+        public void Show(Grid grid) => grid.Children.Add(textBlock);
+        public string Text => textBlock.Text;
+
+        public void Refresh(string textOfBox, ref bool IsEnd)
+        {
+            
+            var text = new StringBuilder(textBlock.Text);
+            if(textOfBox.Length < beginText.Length)
+                if ((text[textOfBox.Length].CompareTo('!') == 0 ||
+                    text[textOfBox.Length].CompareTo('*') == 0) &&
+                    beginText[textOfBox.Length].CompareTo('*') != 0 &&
+                    beginText[textOfBox.Length].CompareTo('!') != 0)
+                {
+                    text[textOfBox.Length] = beginText[textOfBox.Length];
+                    textBlock.Text = text.ToString();
+                    return;
+                }   
+            if (text[textOfBox.Length - 1].CompareTo(textOfBox.Last()) == 0)
+                text[textOfBox.Length - 1] = '*';
+            else
+            {
+                text[textOfBox.Length - 1] = '!';
+                wrongs++;
+            }
+            textBlock.Text = text.ToString();
+            if (textOfBox.Length == beginText.Length)
+                IsEnd = true;
+        }
+    }
+}
---
+++ UrFU_Prokect/UrFU_Prokect/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Elements;
+
+namespace UrFU_Prokect
+{
+    public partial class MainWindow : Window
+    {
+        DateTime StartTime;
+        MyTextBox TextBox;
+        MyTextBlock TextBlock;
+        bool IsEnd = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var buttons = new Buttons();
+            foreach (Button button in buttons)
+            {
+                button.Click += new RoutedEventHandler(ButtonClick);
+                this.Grid.Children.Add(button);
+            }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Grid.Children.Clear();
+            StartTime = DateTime.Now;
+            TextBlock = new MyTextBlock();
+            TextBox = new MyTextBox();
+            TextBox.SetChager(TextBox_TextChanged);
+            TextBlock.SetText(e.Source.ToString().Last());
+            TextBox.Show(this.Grid);
+            TextBlock.Show(this.Grid);
+            TextBox.Focus();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBlock.Refresh(TextBox.Text, ref IsEnd);
+            if(IsEnd)
+            {
+                this.Grid.Children.Clear();
+                ShowResult();
+            }
+        }
+
+        private void ShowResult()
+        {
+            var timeBlock = new TextBlock();
+            timeBlock.FontSize = 20;
+            timeBlock.Margin = new Thickness(350, 100, 300, 200);
+            timeBlock.Text = Time();
+            var wrongsBlock = new TextBlock();
+            wrongsBlock.FontSize = 20;
+            wrongsBlock.Margin = new Thickness(400, 200, 300, 100);
+            wrongsBlock.Text = TextBlock.wrongs.ToString();
+            this.Grid.Children.Clear();
+            this.Grid.Children.Add(timeBlock);
+            this.Grid.Children.Add(wrongsBlock);
+        }
+
+        private string Time()
+        {
+            TimeSpan elapsed = DateTime.Now - StartTime;
+            string text = "";
+            int tenths = elapsed.Milliseconds / 100;
+            text +=
+                elapsed.Hours.ToString("00") + ":" +
+                elapsed.Minutes.ToString("00") + ":" +
+                elapsed.Seconds.ToString("00") + "." +
+                tenths.ToString("0");
+            return text;
+        }
+    }
+}
